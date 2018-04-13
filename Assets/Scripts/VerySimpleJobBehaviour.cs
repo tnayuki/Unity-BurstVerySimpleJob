acad8d16bd079e73bdc400de1687f2b8beb585cd
Toru Nayuki <tnayuki@icloud.com>
2018-04-13 12:23:57 +0900

    Initial commit

---
+++ Assets/Scripts/VerySimpleJobBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Unity.Jobs;
+
+public class VerySimpleJobBehaviour : MonoBehaviour {
+	[ComputeJobOptimization]
+	struct VerySimpleJob : IJob {
+		public int max;
+
+		public long result;
+
+		public void Execute() {
+			for (int i = 0; i < max; i++) {
+				result += i;
+			}
+		}
+	}
+
+	private JobHandle jobHandle;
+	private bool completed;
+
+	void Start() {
+		VerySimpleJob job = new VerySimpleJob() {
+			max = 65536 * 256 * 127
+		};
+
+		jobHandle = job.Schedule();
+	}
+
+	void Update() {
+		if  (!completed && jobHandle.IsCompleted) {
+			Debug.Log(Time.frameCount);
+
+			completed = true;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Burstコンパイラーを用いたとてもシンプルなジョブのサンプル
+
+## 動作条件
+
+実行するには以下のURLからダウンロードしたBurstコンパイラー対応ビルドのUnityが必要です。
+
+[Burstコンパイラー対応ビルド](https://beta.unity3d.com/download/ed1bf90b40e6/public_download.html)
